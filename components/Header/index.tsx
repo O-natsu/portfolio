f69144ff5308e6c5236cfde07b6def52d81d6443
O-natsu <ym.natsubori@gmail.com>
2021-07-29 23:59:17 +0900

    chore: update Header Component

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import styled from "@emotion/styled";
 
 interface Props {
-  link: {
-    to: string;
-    name: string;
-  };
+  // link: {
+  //   to: string;
+  //   name: string;
+  // };
 }
 
-export const Header = React.memo((props) => {
-  return <HeaderBase></HeaderBase>;
+export const Header = React.memo(() => {
+  return <HeaderBase>{'Header'}</HeaderBase>;
 });
 
 const HeaderBase = styled.header`
